8790e722f753d21713024ce7b84c1e946a8c71a7
mixedmercury <112225985+mixedmercury@users.noreply.github.com>
2023-10-04 00:19:35 -0400

    Saving complete

--- Assets/Scripts/SaveSystemTest.cs
+++ Assets/Scripts/SaveSystemTest.cs
@@ -6,42 +6,27 @@ public class SaveSystemTest : MonoBehaviour
 {
     public int testData = 1;
     
-    void Update()
+    public void LoadGame()
     {
-        if (Input.GetKeyDown("[")) //Add value to test variable
-        {
-            testData += 1;
-            Debug.Log(testData);
-        }
-        if (Input.GetKeyDown("o")) //Debug input to test save
-        {
-            SaveGame();
-        }
+        PlayerData data = SaveSystem.LoadPlayer();
 
-        if (Input.GetKeyDown("p")) //Debug input to test loading
-        {
-            PlayerData data = SaveSystem.LoadPlayer();
-
-            testData = data.testData;
-            Vector3 position;
-            position.x = data.position[0];
-            position.y = data.position[1];
-            position.z = data.position[2];
-            transform.position = position;
-            Debug.Log(position);
-        }
-
-        if(Input.GetKeyDown("]"))
-        {
-            //Debug.Log("Player position before change:" + transform.position);
-            transform.position = new Vector3(0,0,0);
-            //Debug.Log("Player Moved to (0, 0, 0)");
-            //Debug.Log("Player position after change:" + transform.position);
-        }
+        testData = data.testData;
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+        Debug.Log(position);
     }
 
     public void SaveGame()
     {
         SaveSystem.SavePlayer(this);
+    }
+
+    public void TestValue()
+    {
+        testData += 1;
+        Debug.Log(testData);
     }
 }
