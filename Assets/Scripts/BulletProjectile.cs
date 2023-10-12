3bb12a040a912a58ed846fa5f00f21a212a0c820
Jesus Leal <je008561@ucf.edu>
2023-10-11 21:07:17 -0400

    HitBox Fix
    HitBox Fix

--- Assets/Scripts/BulletProjectile.cs
+++ Assets/Scripts/BulletProjectile.cs
@@ -28,24 +28,6 @@ public class BulletProjectile : MonoBehaviour
                 healthMetrics.ModifyHealth(-20f);// Apply 20 damage to the object
                 Debug.Log("A WeakPoint");
             }
-            // Handle the hit target logic here, if needed.
-
-            else
-            {
-                healthMetrics.ModifyHealth(-10f);// Apply 20 damage to the object
-                Debug.Log("Not a WeakPoint");
-            }
-        }
-
-        else
-        {
-            HealthMetrics healthMetrics = other.GetComponent<HealthMetrics>();
-
-            if (healthMetrics != null)
-            {
-                healthMetrics.ModifyHealth(-10f);// Apply 20 damage to the object
-                Debug.Log("Not a WeakPoint");
-            }
         }
 
         Destroy(gameObject);
