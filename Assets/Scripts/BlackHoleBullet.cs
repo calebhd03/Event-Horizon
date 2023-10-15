3aa5bad2fff0846aa9a4c3371f40ae943137e58a
mixedmercury <112225985+mixedmercury@users.noreply.github.com>
2023-10-15 04:21:45 -0400

    Black hole enemy detection and scaling effects

--- Assets/Scripts/BlackHoleBullet.cs
+++ Assets/Scripts/BlackHoleBullet.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class BlackHoleBullet : MonoBehaviour
 {
     private Rigidbody bulletRigidbody;
+    private Renderer bulletRenderer;
+    public int eventHorizonRadius;
+    public float growthTime;
 
     private void Awake()
     {
@@ -15,10 +18,46 @@ public class BlackHoleBullet : MonoBehaviour
     {
         float speed = 50f;
         bulletRigidbody.velocity = transform.forward * speed;
+        Object.Destroy(gameObject,2.0f);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        bulletRigidbody.constraints = RigidbodyConstraints.FreezePosition;
+        StartCoroutine(ScaleOverTime(growthTime));
+        transform.localScale = new Vector3 (eventHorizonRadius, eventHorizonRadius, eventHorizonRadius);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, eventHorizonRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if(hitCollider.tag == "Enemy")
+            {
+                Debug.Log("Woah an enemy!");
+            }
+        }
+    }
+
+    IEnumerator ScaleOverTime(float time)
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 destinationScale = new Vector3(eventHorizonRadius, eventHorizonRadius, eventHorizonRadius);
+        float currentTime = 0.0f;
+
+        while(currentTime <= time)
+        {
+            Color objectColor = GetComponent<Renderer>().material.color;
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, 1 / (currentTime / time));
+            GetComponent<Renderer>().material.color = objectColor;
+            transform.localScale = Vector3.Lerp(destinationScale, originalScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, eventHorizonRadius);
     }
 }
