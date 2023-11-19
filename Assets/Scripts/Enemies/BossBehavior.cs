a2954d40a529f19716fec5ca895a0de2c5eccdb0
Jesus Leal <je008561@ucf.edu>
2023-11-19 02:57:50 -0500

    meteor

--- Assets/Scripts/Enemies/BossBehavior.cs
+++ Assets/Scripts/Enemies/BossBehavior.cs
@@ -9,8 +9,25 @@ public class BossBehavior : MonoBehaviour
     public Transform player;
     public NavMeshAgent agent;
 
-    public Transform[] movePoints;
-    private int destinationPoints = 0;
+    //layerCheck
+    public LayerMask playerZone;
+
+    //See
+    private bool iSeeYou;
+    public float seeDistance;
+
+    //Meteor Attack
+    public GameObject meteorPrefab;
+    public Transform rightMeteor;
+    public Transform leftMeteor;
+    public Transform middleMeteor;
+    public float meteorWindUp;
+    public float timeBetweenMeteorAttack;
+    public float meteorSpeed = 10;
+    private bool meteorAttack = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,24 +38,42 @@ public class BossBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!agent.pathPending && agent.remainingDistance < 0.1f)
+        iSeeYou = Physics.CheckSphere(transform.position, seeDistance, playerZone);
+        if(iSeeYou == true && meteorAttack == false)
         {
-
-            pointMovement();
-            
+            transform.LookAt(player);
+            StartCoroutine(PerformAttack());
         }
     }
 
-    public void pointMovement()
+    IEnumerator PerformAttack()
     {
-        //resets movement
-        if (movePoints.Length == 0)
-            return;
+        meteorAttack = true;
 
+        yield return new WaitForSeconds(meteorWindUp);
 
-        agent.destination = movePoints[destinationPoints].position;
+        summonMeteor(rightMeteor.position, Quaternion.identity);
+        yield return new WaitForSeconds(timeBetweenMeteorAttack);
 
-        destinationPoints = (destinationPoints + 1) % movePoints.Length;
-        Debug.Log("moving to " + agent.destination);
+        summonMeteor(leftMeteor.position, Quaternion.identity);
+        yield return new WaitForSeconds(timeBetweenMeteorAttack);
+
+        summonMeteor(middleMeteor.position, Quaternion.identity);
+
+        meteorAttack = false;
+        
+    }
+
+    public void summonMeteor(Vector3 position, Quaternion rotation)
+    {
+        Rigidbody newMeteor = Instantiate(meteorPrefab, position, rotation).GetComponent<Rigidbody>();
+        Vector3 directionToPlayer = (player.position - position).normalized;
+        newMeteor.velocity = directionToPlayer * meteorSpeed;
+        Destroy(newMeteor.gameObject, 5f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, seeDistance);
     }
 }
