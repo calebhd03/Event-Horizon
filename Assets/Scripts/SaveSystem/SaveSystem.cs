b1f2862d445ae0843110a96fdf78a4521e5554ff
Aeon-Domini <aeon.charfauros@knights.ucf.edu>
2023-11-19 19:31:38 -0500

    Player Respawn working

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -50,20 +50,47 @@ public static class SaveSystem
         return null;
     }
 
-    public static void SavePlayer(PlayerSaveData saveData)
+public static void SavePlayer(PlayerSaveData saveData)
     {
         SavePlayerData(saveData.playerData);
         SavePlayerAmmoData(saveData.ammoData);
+        SavePlayerHealth(saveData.healthData);
     }
 
     public static PlayerSaveData LoadPlayer()
     {
         PlayerData playerData = LoadPlayerData();
         PlayerAmmoData ammoData = LoadPlayerAmmoData();
+        PlayerHealth healthData = LoadPlayerHealth();
 
-        if (playerData != null && ammoData != null)
+        if (playerData != null && ammoData != null && healthData != null)
         {
-            return new PlayerSaveData(playerData, ammoData);
+            return new PlayerSaveData(playerData, ammoData, healthData);
+        }
+        return null;
+    }
+
+    private static void SavePlayerHealth(PlayerHealth health)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/playerHealth.data";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, health);
+        stream.Close();
+        Debug.Log("Health saved: ");
+    }
+
+    private static PlayerHealth LoadPlayerHealth()
+    {
+        string path = Application.persistentDataPath + "/playerHealth.data";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            PlayerHealth health = formatter.Deserialize(stream) as PlayerHealth;
+            stream.Close();
+            return health;
+            Debug.Log("Health loaded");
         }
         return null;
     }
