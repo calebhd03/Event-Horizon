c610856bd16ff938c64962f5bb16fb6e2bf58d93
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2023-11-24 12:35:22 -0500

    log system should change pictures now

--- Assets/Scripts/LogSystem/LogSystem.cs
+++ Assets/Scripts/LogSystem/LogSystem.cs
@@ -8,6 +8,8 @@ public class LogSystem : MonoBehaviour
     [SerializeField] GameObject enemiesButton, memoriesButton, itemButton, returnButton;
     [SerializeField] GameObject enemiesPage, memoriesPage, itemsPage, pauseMenu, LogPage;
     [SerializeField] Button[] enemy, memory, item;
+    private Image[] enemyImage, memoryImage, itemImage;
+    [SerializeField] Sprite[] enemySprite, memorySprite, itemSprite;
     public static int currentTab;
 
     void Start()
@@ -26,6 +28,10 @@ public class LogSystem : MonoBehaviour
         {
             button.interactable = false;
         }
+        
+        enemyImage = new Image[enemy.Length];
+        memoryImage = new Image[memory.Length];
+        itemImage = new Image[item.Length];
     }
 
     void Update()
@@ -74,17 +80,33 @@ public class LogSystem : MonoBehaviour
     public void UpdateEnemyLog()
     {
         EnemiesScanScript enemiesScanScript = FindObjectOfType<EnemiesScanScript>();
+        if (enemiesScanScript.number >= 0 && enemiesScanScript.number < enemyImage.Length)
+        {
+        enemy[enemiesScanScript.number].image.sprite = enemySprite[enemiesScanScript.number];
         enemy[enemiesScanScript.number].interactable = true;
+        }
+    else
+    {
+        Debug.LogError("Invalid enemy index: " + enemiesScanScript.number);
+    }
     }
     public void UpdateMemoryLog()
     {
         ObjectivesScript objectivesScript = FindObjectOfType<ObjectivesScript>();
+        if (objectivesScript.number >= 0 && objectivesScript.number < enemyImage.Length)
+        {
+        memory[objectivesScript.number].image.sprite = memorySprite[objectivesScript.number];
         memory[objectivesScript.number].interactable = true;
+        }
     }
     public void UpdateItemLog()
     {
         ItemsScript itemsScript = FindObjectOfType<ItemsScript>();
+        if (itemsScript.number >= 0 && itemsScript.number < enemyImage.Length)
+        {
+        item[itemsScript.number].image.sprite = itemSprite[itemsScript.number];
         item[itemsScript.number].interactable = true;
+        }
     }
 
     public void TestButton()
