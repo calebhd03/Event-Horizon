ce239bad28008523733ad7a6769ea02496f812a8
Aeon-Domini <aeon.charfauros@knights.ucf.edu>
2023-11-26 15:29:48 -0500

    progress spawn enemies

--- Assets/Scripts/Progress.cs
+++ Assets/Scripts/Progress.cs
@@ -16,10 +16,29 @@ public class Progress : MonoBehaviour
     public GameObject progressBar; // Assign the ProgressBar GameObject in the Inspector
     public GameObject portal; // Assign the Portal GameObject in the Inspector
 
+    private bool enemy1Activated = false;
+    private bool enemy2Activated = false;
+    private bool enemy3Activated = false;
+
+    public GameObject enemyPrefab1; // Assign the enemy prefabs in the Inspector
+    public GameObject enemyPrefab2;
+    public GameObject enemyPrefab3;
+
+    private GameObject enemyInstance1;
+    private GameObject enemyInstance2;
+    private GameObject enemyInstance3;
+
     private float currentProgress = 0f;
     private bool isPlayerInside = false;
+    private bool PlayerStillInside = false;
     private bool countdownStopped = false;
     private bool countdownStarted = false;
+
+    public Vector3 spawnPosition1; // Replace with your actual spawn positions
+    public Vector3 spawnPosition2;
+    public Vector3 spawnPosition3;
+
+    private bool resettingProgress = false;
 
     private void Start()
     {
@@ -30,6 +49,9 @@ public class Progress : MonoBehaviour
         {
             SetProgressBarScale(0f); // Set the initial scale of the progress bar to 0
         }
+
+        // Instantiate initial enemies
+        InstantiateEnemies();
     }
 
     private void Update()
@@ -48,6 +70,12 @@ public class Progress : MonoBehaviour
             if (currentProgress >= 1.0f)
             {
                 portal.SetActive(true);
+
+                // Check if progress is not being reset before instantiating new enemies
+                if (!resettingProgress)
+                {
+                    InstantiateEnemies();
+                }
             }
         }
         else
@@ -59,6 +87,17 @@ public class Progress : MonoBehaviour
         }
 
         UpdateHighlight();
+        ActivateEnemies();
+
+        if (enemy1Activated && enemyInstance1 == null && !enemy2Activated)
+        {
+            ActivateEnemy2();
+        }
+
+        if (enemy2Activated && enemyInstance2 == null && !enemy3Activated)
+        {
+            ActivateEnemy3();
+        }
     }
 
     private void UpdateHighlight()
@@ -121,6 +160,8 @@ public class Progress : MonoBehaviour
         if (other.gameObject.CompareTag(playerTag))
         {
             isPlayerInside = true;
+            PlayerStillInside = true;
+            
         }
         else if (other.gameObject.CompareTag(enemyTag))
         {
@@ -133,6 +174,132 @@ public class Progress : MonoBehaviour
         if (other.gameObject.CompareTag(playerTag))
         {
             isPlayerInside = false;
+            PlayerStillInside = false;
+        }
+        else if (other.gameObject.CompareTag(enemyTag) && PlayerStillInside)
+        {
+            isPlayerInside = true;
+        }
+    
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+      if (collision.gameObject.CompareTag(enemyTag) && PlayerStillInside)
+      {
+            isPlayerInside = true;
+      }
+
+    }
+
+    private void ActivateEnemies()
+    {
+         float progressThreshold1 = 1f / 9f;
+        if (!enemy1Activated && currentProgress >= progressThreshold1)
+        {
+            if (enemyInstance1 != null)
+            {
+                enemyInstance1.SetActive(true);
+                enemy1Activated = true;
+            }
+        }
+    }
+
+    private void ActivateEnemy2()
+    {
+        float progressThreshold2 = 0.4f; // Activate enemy2 at 2/5 progress
+        if (!enemy2Activated && currentProgress >= progressThreshold2)
+        {
+            if (enemyInstance2 != null)
+            {
+                enemyInstance2.SetActive(true);
+                enemy2Activated = true;
+            }
+        }
+    }
+
+    private void ActivateEnemy3()
+    {
+        float progressThreshold3 = 0.6f; // Activate enemy3 at 3/5 progress
+        if (!enemy3Activated && currentProgress >= progressThreshold3)
+        {
+            if (enemyInstance3 != null)
+            {
+                enemyInstance3.SetActive(true);
+                enemy3Activated = true;
+            }
+        }
+    }
+
+    private void InstantiateEnemies()
+    {
+        // Instantiate initial enemies
+        if (enemyPrefab1 != null)
+        {
+            enemyInstance1 = Instantiate(enemyPrefab1, spawnPosition1, Quaternion.identity);
+            enemyInstance1.SetActive(false);
+        }
+
+        if (enemyPrefab2 != null)
+        {
+            enemyInstance2 = Instantiate(enemyPrefab2, spawnPosition2, Quaternion.identity);
+            enemyInstance2.SetActive(false);
+        }
+
+        if (enemyPrefab3 != null)
+        {
+            enemyInstance3 = Instantiate(enemyPrefab3, spawnPosition3, Quaternion.identity);
+            enemyInstance3.SetActive(false);
+        }
+    }
+
+    public void ResetProgress()
+    {
+        Debug.Log("Progress Reset");
+
+        // Set the flag to indicate that progress is being reset
+        resettingProgress = true;
+
+        // Reset progress-related variables and destroy existing enemies
+        currentProgress = 0f;
+        enemy1Activated = false;
+        enemy2Activated = false;
+        enemy3Activated = false;
+
+        DestroyExistingEnemies();
+
+        // Reset progress time to the original value
+        progressTime = OriginalProgressTime;
+
+        if (portal != null)
+        {
+            portal.SetActive(false);
+        }
+
+        // Reset countdown if needed
+        countdownStopped = false;
+        countdownStarted = false;
+
+        // Reset the flag after everything is done
+        resettingProgress = false;
+    }
+
+    private void DestroyExistingEnemies()
+    {
+        // Destroy existing enemy instances if they exist
+        if (enemyInstance1 != null)
+        {
+            Destroy(enemyInstance1);
+        }
+
+        if (enemyInstance2 != null)
+        {
+            Destroy(enemyInstance2);
+        }
+
+        if (enemyInstance3 != null)
+        {
+            Destroy(enemyInstance3);
         }
     }
 }
