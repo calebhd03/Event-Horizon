8e58ffdf7c53317f25402df568ce6cb4ff494a62
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2023-11-28 20:18:04 -0500

    Checkpoints play sound

--- Assets/Scripts/SaveSystem/CheckpointSave.cs
+++ Assets/Scripts/SaveSystem/CheckpointSave.cs
@@ -9,14 +9,23 @@ public class CheckpointSave : MonoBehaviour
     public TMP_Text objectiveText;
     public string objective;
     private SaveSystemTest saveSystemTest;
+    AudioSource audioSource;
+    public AudioClip audioClip;
 
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void OnTriggerEnter(Collider other)
     {
+
         saveSystemTest = other.GetComponent<SaveSystemTest>();
         if (objectiveTriggered  == false)
         {
             objectiveText.SetText(objective);
             objectiveTriggered = true;
+            audioSource.clip = audioClip;
+            audioSource.PlayOneShot(audioClip);
         }
         if (saveSystemTest != null)
         {
