aad0c030c0f7b034ca352321f60e202afdf79f8f
Jesus Leal <je008561@ucf.edu>
2024-01-27 21:38:39 -0500

    Plant AI

---
+++ Assets/Scripts/Enemies/plantCloudDamage.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantCloudDamage : MonoBehaviour
+{
+    public float cloudDamage = 2f;
+    public float damageInterval = 1f; // Time interval between damage applications
+    private float timer = 0f;
+    private bool playerInsideCloud = false;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (playerInsideCloud)
+        {
+            timer += Time.deltaTime;
+            if (timer >= damageInterval)
+            {
+                timer = 0f;
+                ApplyDamage();
+            }
+        }
+    }
+
+    private void ApplyDamage()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            PlayerHealthMetric playerHealthMetric = player.GetComponent<PlayerHealthMetric>();
+
+            if (playerHealthMetric != null)
+            {
+                playerHealthMetric.ModifyHealth(-cloudDamage);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInsideCloud = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInsideCloud = false;
+        }
+    }
+}
