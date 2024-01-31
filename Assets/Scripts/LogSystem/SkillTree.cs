c79256aa6741bda31d0f0444a4c65e3964b76308
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2024-01-30 23:07:56 -0500

    upgrade system messes with values

--- Assets/Scripts/LogSystem/SkillTree.cs
+++ Assets/Scripts/LogSystem/SkillTree.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
 
 public class SkillTree : MonoBehaviour
 {
+    PlayerHealthMetric playerHealthMetric;
+    ThirdPersonController thirdPersonController;
+    regularPoint regularPoint;
+    weakPoint weakPoint;
+    ScanCam scanCam;
+    public float healthUpgradeAmount, speedUpgradedAmount, damageUpgradeAmount;
+    
     void Start()
     {
-        
+        playerHealthMetric = FindObjectOfType<PlayerHealthMetric>();
+        thirdPersonController = FindObjectOfType<ThirdPersonController>();
+        scanCam = FindObjectOfType<ScanCam>();
+        regularPoint = scanCam.scannerCurrentObject.GetComponent<regularPoint>();
+        weakPoint = scanCam.scannerCurrentObject.GetComponent<weakPoint>();
+    }
+    public void HealthUpgraded()
+    {
+        playerHealthMetric.maxHealth = playerHealthMetric.maxHealth * healthUpgradeAmount;
+    }
+    
+    public void SpeedUpgraded()
+    {
+        thirdPersonController.MoveSpeed = thirdPersonController.MoveSpeed * speedUpgradedAmount;
+        thirdPersonController.SprintSpeed = thirdPersonController.SprintSpeed * speedUpgradedAmount;
     }
 
-    void Update()
+    public void DamageUpgraded()
     {
-        
+        //regularPoint.regularDamage = regularPoint.regularDamage * damageUpgradeAmount;
+        //weakPoint.weakPointDamage = weakPoint.weakPointDamage * damageUpgradeAmount;
+        regularPoint.damageUpgrade = true;
+        weakPoint.damageUpgrade = true;
     }
 }
