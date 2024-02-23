d1b293d9c8b002fe7fdb964659aab2dd7824083e
Jesus Leal <je008561@ucf.edu>
2024-02-22 21:12:38 -0500

    Flying and Exploding Plant

---
+++ Assets/Scripts/Enemies/plant/ExplodingPlant/ExplodingPlant.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingPlant : MonoBehaviour
+{
+    public Animator animator;
+    private Rigidbody rb;
+    private bool iSeeYou;
+    [SerializeField] EnemyHealthBar healthBar;
+    [SerializeField] private HealthMetrics healthMetrics;
+    public LayerMask playerZone;
+    public float triggerDistance;
+
+    [Header("Exploding Variables")]
+    public GameObject acidPrefab;
+    private bool explode = false;
+    public float explodeAnimationDuration;
+    private bool acidSpawned = false;
+
+
+    [Header("Audio")]
+    public AudioClip deathAudio;
+    AudioSource audioSource;
+
+    [Header("Drops")]
+    public GameObject blasterPickupPrefab;
+    public GameObject shotGunPickupPrefab;
+    public GameObject bHPickupPrefab;
+    public GameObject healthPickupPrefab;
+    public float pickupDropChance = 0.3f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        healthBar = GetComponentInChildren<EnemyHealthBar>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthMetrics = GetComponentInParent<HealthMetrics>();
+        healthMetrics.currentHealth = healthMetrics.maxHealth;
+        healthBar.updateHealthBar(healthMetrics.currentHealth, healthMetrics.maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHealth();
+        iSeeYou = Physics.CheckSphere(transform.position, triggerDistance, playerZone);
+
+        if (iSeeYou)
+        {
+            TriggerPlant();
+        }
+    }
+
+    private void TriggerPlant()
+    {
+        explode = true;
+        if(explode)
+        {
+            StartCoroutine(ExplodeDelay());
+        }
+    }
+
+    private IEnumerator ExplodeDelay()
+    {
+        //explode animation and spread acid
+        yield return new WaitForSeconds(explodeAnimationDuration);
+        
+        if(!acidSpawned)
+        {
+            acidSpawned = true;
+            SpawnAcid();
+        }
+    }
+
+    private void SpawnAcid()
+    {
+        Debug.Log("Spawn acid");
+        GameObject newAcidCloud = Instantiate(acidPrefab, transform.position, Quaternion.identity);
+        Destroy(newAcidCloud.gameObject, 10f);
+        Destroy(this.gameObject, 10f);
+    }
+
+    public void OneShotTop()
+    {
+        TriggerPlant();
+    }
+
+    public void shotButtom()
+    {
+        Die();
+    }
+    public void updateHealth()
+    {
+        healthMetrics = GetComponentInParent<HealthMetrics>();
+        healthBar.updateHealthBar(healthMetrics.currentHealth, healthMetrics.maxHealth);
+
+        if (healthMetrics.currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        // Trigger the death animation which is the plant explosion animation
+        StartCoroutine(WaitAndDropStuff(3f));
+    }
+
+    private IEnumerator WaitAndDropStuff(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        audioSource.PlayOneShot(deathAudio);
+
+        // Call DropStuff after waiting for 3 seconds
+        DropStuff();
+    }
+
+    private void DropStuff()
+    {
+        if (Random.value < pickupDropChance)
+        {
+            Instantiate(shotGunPickupPrefab, transform.position, Quaternion.identity);
+            Instantiate(blasterPickupPrefab, transform.position, Quaternion.identity);
+            Instantiate(bHPickupPrefab, transform.position, Quaternion.identity);
+        }
+
+        if (Random.value < pickupDropChance / 2)
+        {
+            Instantiate(healthPickupPrefab, transform.position, Quaternion.identity);
+        }
+
+        Destroy(transform.parent.gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, triggerDistance);
+    }
+}
