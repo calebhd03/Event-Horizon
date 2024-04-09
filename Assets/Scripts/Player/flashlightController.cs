81730e6caeb9f7a39f31f27133fb8535d64dd089
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2024-04-09 13:24:04 -0400

    cant flashlight in menus

--- Assets/Scripts/Player/flashlightController.cs
+++ Assets/Scripts/Player/flashlightController.cs
@@ -8,10 +8,19 @@ public class flashlightController : MonoBehaviour
     private StarterAssetsInputs starterAssetsInputs;
     [SerializeField] private GameObject flashlightObject;
     private bool flashlightActive = false;
+    //added to remove flashlight from use in menus
+    [SerializeField]PauseMenuScript pauseMenuScript;
+    [SerializeField]LogSystem logSystem;
+    [SerializeField]Scanning scanning;
+    [SerializeField]MiniCore miniCore;
 
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        miniCore = GetComponentInParent<MiniCore>();
+        scanning = miniCore.GetComponentInChildren<Scanning>();
+        logSystem = miniCore.GetComponentInChildren<LogSystem>();
+        pauseMenuScript = miniCore.GetComponentInChildren<PauseMenuScript>();
     }
     // Start is called before the first frame update
     void Start()
@@ -22,7 +31,7 @@ public class flashlightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(starterAssetsInputs.flashlight == true)
+        if(starterAssetsInputs.flashlight == true && scanning.Scan == false && logSystem.log == false && pauseMenuScript.paused == false)
         {
             if (flashlightActive == false)
             {
@@ -31,7 +40,7 @@ public class flashlightController : MonoBehaviour
             }
         }
 
-        if (starterAssetsInputs.flashlight == false)
+        if (starterAssetsInputs.flashlight == false && scanning.Scan == false && logSystem.log == false && pauseMenuScript.paused == false)
         {
             flashlightObject.gameObject.SetActive(false);
             starterAssetsInputs.flashlight = false;
