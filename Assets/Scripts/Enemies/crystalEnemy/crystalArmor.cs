d9acd2e138febfd565c6b35c8e2a02fab46de9ce
calebhd03 <calebhd03@gmail.com>
2024-04-22 12:58:15 -0400

    added extra steps to the crystal guard armour

--- Assets/Scripts/Enemies/crystalEnemy/crystalArmor.cs
+++ Assets/Scripts/Enemies/crystalEnemy/crystalArmor.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class crystalArmor : MonoBehaviour
 {
+    [SerializeField] int armorMaxHealth;
     private int shotOnArmor = 0;
     [SerializeField] crystalEnemy crystalEnemy;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] AudioSource breakSound;
+    [SerializeField] ParticleSystem breakParticle;
+    [SerializeField] AudioSource hitSound;
+    [SerializeField] GameObject model;
+    [SerializeField] AudioClip[] crystalHitSounds;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            shotOnArmor += 1;
+            shotOnArmor += 1; 
+            UpdateArmorHealth();
             Debug.Log("Shots on armor" + shotOnArmor);
         }
     }
 
     private void UpdateArmorHealth()
     {
-        if (shotOnArmor == 4)
+        if (shotOnArmor == armorMaxHealth)
         {
+            model.SetActive(false);
+            GetComponent<Collider>().enabled = false;
+
+            if (breakSound != null)  breakSound?.Play();
+            if (breakParticle != null) breakParticle?.Play();
+
+            
             crystalEnemy.ArmorBroke();
+        }
+        else
+        {
+            if(hitSound != null) hitSound.PlayOneShot(crystalHitSounds[Random.Range(0, crystalHitSounds.Length-1)]);
         }
     }
 }
