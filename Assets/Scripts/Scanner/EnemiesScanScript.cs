368b1b0cfe6c12af905bbdaeab0f5de7971c2e4b
calebhd03 <calebhd03@gmail.com>
2024-04-23 13:22:50 -0400

    Revert "enemies scannable"
    This reverts commit c52832335d96a6e42d8b4b6e107a8074bdf1f043.

--- Assets/Scripts/Scanner/EnemiesScanScript.cs
+++ Assets/Scripts/Scanner/EnemiesScanScript.cs
@@ -21,7 +21,6 @@ public class EnemiesScanScript : MonoBehaviour
 
     private AudioSource alertSound;
     public SkinnedMeshRenderer skinnedMeshRenderer;
-    public MeshRenderer meshRenderer;
     public Material[] materials;
     ScanCam scanCam;
     public GameObject player;
@@ -36,12 +35,6 @@ public class EnemiesScanScript : MonoBehaviour
         if(skinnedMeshRenderer != null)
         {
             materials = skinnedMeshRenderer.materials;
-            materials[1].SetFloat("_isHovered", 1);
-            materials[1].SetFloat("_isHighlighted", 0);
-        }
-        else if(meshRenderer != null)
-        {
-            materials = meshRenderer.materials;
             materials[1].SetFloat("_isHovered", 1);
             materials[1].SetFloat("_isHighlighted", 0);
         }
