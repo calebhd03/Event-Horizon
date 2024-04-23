39230c1c4ab6fc9413f94b22a9cd5b789bd39bcf
Aeon-Domini <aeon.charfauros@knights.ucf.edu>
2024-04-23 16:56:04 -0400

    stuff for julian

---
+++ Assets/Scenes/Code Dev Scenes/AeonCode Dev/Aeon's Stuff/TriggeredGameObject.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredGameObject : MonoBehaviour
+{
+    public float heightIncrease = 10.0f; // Amount of units to increase height
+    public float spinSpeedSlow = 3.0f; // Initial speed at which the object spins around the Y-axis
+    public float spinSpeedFast = 30.0f; // Speed after 19 seconds
+    public GameObject explosionPrefab; // Particle effect prefab
+    public AudioClip explosionSound; // Sound to play on explosion
+
+    private bool triggered = false;
+    private bool speedingUp = false;
+    private Rigidbody rb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered && other.CompareTag("Player"))
+        {
+            triggered = true;
+
+            // Increase height
+            Vector3 newPosition = transform.position;
+            newPosition.y += heightIncrease;
+            transform.position = newPosition;
+
+            // Start spinning
+            StartSpinning(spinSpeedSlow);
+
+            // Invoke method to speed up spinning after 19 seconds
+            Invoke("SpeedUpSpinning", 19.0f);
+
+            // Instantiate particle effect after a delay
+            Invoke("Explode", 19.0f);
+
+            // Play sound effect
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+
+            // Destroy this object after a delay
+            Destroy(gameObject, 20.0f);
+        }
+    }
+
+    private void StartSpinning(float speed)
+    {
+        // Start spinning around the Y-axis
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+            rb.isKinematic = true;
+        }
+        rb.angularVelocity = Vector3.up * speed;
+    }
+
+    private void SpeedUpSpinning()
+    {
+        if (rb != null)
+        {
+            rb.angularVelocity = Vector3.up * spinSpeedFast;
+        }
+    }
+
+    private void Explode()
+    {
+        // Instantiate particle effect
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+    }
+}
